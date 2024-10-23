721ba56ca194ef8db65b3b328f0de0e21a86c11d
degerahmet <adeger34@gmail.com>
2024-10-22 19:28:20 -0700

    module not found error when running the build command

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/constants/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/hooks/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/props/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     screens: {
